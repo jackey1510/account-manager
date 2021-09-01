e1af03458377f4fa37d3cb7ac240be4201dedea8
jackey <jackey1510@gmail.com>
2021-09-02 00:12:58 +0800

    add annotations and toDto methods

--- src/main/kotlin/com/acmebank/account/manager/model/Account.kt
+++ src/main/kotlin/com/acmebank/account/manager/model/Account.kt
@@ -1,12 +1,17 @@
 package com.acmebank.account.manager.model
 
+import com.acmebank.account.manager.dto.response.BalanceResponseDto
 import java.math.BigDecimal
 import java.util.Date
+import javax.persistence.*
 
+@Entity
 data class Account(
-        var accountNumber: String = "",
-        var balance: BigDecimal = BigDecimal(0),
-        var currency: String = "HKD",
-        var createdAt: Date = Date(),
-        var updatedAt: Date = Date()
+                @Id var accountNumber: String = "",
+                var balance: BigDecimal = BigDecimal(0),
+                var currency: String = "HKD",
+                var createdAt: Date = Date(),
+                var updatedAt: Date = Date()
 )
+
+fun Account.toBalanceResponseDto() = BalanceResponseDto(balance, currency)
--- src/main/kotlin/com/acmebank/account/manager/model/TransactionLog.kt
+++ src/main/kotlin/com/acmebank/account/manager/model/TransactionLog.kt
@@ -3,10 +3,12 @@ package com.acmebank.account.manager.model
 import com.acmebank.account.manager.enum.TransactionStatus
 import java.util.Date
 import java.util.UUID
+import javax.persistence.*
 
+@Entity
 data class TransactionLog(
-                var id: UUID? = null,
-                var tranasctionId: UUID = UUID.randomUUID(),
-                var status: TransactionStatus = TransactionStatus.PENDING,
-                var timestamp: Date = Date(),
+        @Id @GeneratedValue var id: UUID? = null,
+        var tranasctionId: UUID = UUID.randomUUID(),
+        var status: TransactionStatus = TransactionStatus.PENDING,
+        var timestamp: Date = Date(),
 )
