8876a4442a930fe70f7be4edc2e7719ca42abd5a
jackey1510 <jackey@HK010401011.local>
2021-09-03 15:09:04 +0800

    get account balance, insert sample accounts, error handler, fix H2 Hibernate issues

---
+++ src/main/kotlin/com/acmebank/account/manager/service/AccountServiceImpl.kt
@@ -0,0 +1,24 @@
+package com.acmebank.account.manager.service
+
+import com.acmebank.account.manager.model.Account
+import com.acmebank.account.manager.repository.AccountRepository
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class AccountServiceImpl : AccountService {
+    private var logger = LoggerFactory.getLogger(AccountService::class.java)
+
+    @Autowired
+    private lateinit var accountRepository: AccountRepository
+
+    override fun getAccountByAccountNumber(accountNumber: String): Account? {
+        return try {
+            accountRepository.getAccountByAccountNumber(accountNumber)
+        } catch (ex: RuntimeException) {
+            logger.error(ex.message)
+            return null
+        }
+    }
+}
---
+++ src/main/resources/db/migration/V002__insert_sample_accounts.sql
@@ -0,0 +1,2 @@
+Insert into ACCOUNT values ('12345678', 1000000,'HKD', NOW(), NOW());
+Insert into ACCOUNT values ('88888888', 1000000,'HKD',NOW(), NOW());
