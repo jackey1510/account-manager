f77ac6cda5d337afc903e6e740a0a89b5a16cd50
jackey <jackey1510@gmail.com>
2021-09-06 00:08:22 +0800

    readme and remove unused files

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
+        freeCompilerArgs = listOf("-Xjsr305=strict", "-Xemit-jvm-type-annotations")
         jvmTarget = "11"
     }
 }
